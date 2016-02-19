b1122ccfc615b3efa7841603e54579efdf8126d4
김영환 <freedate@naver.com>
2016-02-19 18:22:31 +0900

    셋팅용

--- src/main/java/co/kr/doosam/config/WebMvcConfig.java
+++ src/main/java/co/kr/doosam/config/WebMvcConfig.java
@@ -1,5 +1,13 @@
 package co.kr.doosam.config;
 
-public class WebMvcConfig {
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebMvcConfig extends WebMvcAutoConfigurationAdapter{
 
 }
