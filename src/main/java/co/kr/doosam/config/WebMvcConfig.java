766d5cbf12185943aa9326b942420e9734228d83
Q_office <Q_office@115.21.113.35>
2016-02-19 16:08:30 +0900

    리소스핸들러추가

--- src/main/java/co/kr/doosam/config/WebMvcConfig.java
+++ src/main/java/co/kr/doosam/config/WebMvcConfig.java
@@ -1,5 +1,35 @@
 package co.kr.doosam.config;
 
-public class WebMvcConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.wonier.converter"}, excludeFilters=@ComponentScan.Filter(Configuration.class))
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+     
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/").addResourceLocations("/resources/**");
+    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+     
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+     
 }
