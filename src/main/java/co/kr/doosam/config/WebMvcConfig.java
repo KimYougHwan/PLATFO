5cd0d9a3dd783ca8064489200b545fd18927ba55
김영환 <freedate@naver.com>
2016-02-23 10:16:52 +0900

    리소스 설정

--- src/main/java/co/kr/doosam/config/WebMvcConfig.java
+++ src/main/java/co/kr/doosam/config/WebMvcConfig.java
@@ -3,11 +3,22 @@ package co.kr.doosam.config;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebMvcConfig extends WebMvcAutoConfigurationAdapter{
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/resources/").addResourceLocations("/resources/**");
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 }
