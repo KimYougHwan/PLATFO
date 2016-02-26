220ecff02a670282edb5ac960a9fa3518c87f4c4
chohanyong <Administrator@115.21.112.218>
2016-02-26 12:49:43 +0900

    bootstrap수정

--- src/main/java/co/kr/doosam/board/controller/NoticeListController.java
+++ src/main/java/co/kr/doosam/board/controller/NoticeListController.java
@@ -1,21 +1,28 @@
 package co.kr.doosam.board.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import co.kr.doosam.board.dto.NoticeDto;
+import co.kr.doosam.board.dao.NoticeDao;
 import co.kr.doosam.board.service.BoardService;
+
 
 @Controller
 public class NoticeListController {
 		
+	@Autowired
+	BoardService boardService;
 	@RequestMapping(value = "/board/noticeList")
-	public String noticeList(){
+	public ModelAndView noticeList(){
 		
-
-		
+		NoticeDao nDto = new NoticeDao();
+		List<NoticeDao> noticeList = boardService.noticeList();
 		//관리자 공지사항
-		return "board/noticeList";
+		return new ModelAndView("board/noticeList", "noticeList", noticeList);
 	}
 	
 }
