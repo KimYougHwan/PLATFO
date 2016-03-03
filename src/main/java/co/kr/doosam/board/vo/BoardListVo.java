163f1e440e4827caad2243351218c97610f4f540
chohanyong <Administrator@115.21.112.218>
2016-03-03 16:37:04 +0900

    신규

--- src/main/java/co/kr/doosam/board/vo/BoardListVo.java
+++ src/main/java/co/kr/doosam/board/vo/BoardListVo.java
@@ -1,5 +1,71 @@
 package co.kr.doosam.board.vo;
 
-public class BoardListVo {
+import java.sql.Date;
 
+public class BoardListVo {
+	private String  BoardType;
+	private String  BoardNumber;
+	private String  BoardTitle;
+	private String 	BoardComm;
+	private String	userId;
+	private String 	createUser;
+	private Date   	createDate;
+	private String 	modifyUser;
+	private Date   	modifyDate;
+	
+	public String getBoardType() {
+		return BoardType;
+	}
+	public void setBoardType(String boardType) {
+		BoardType = boardType;
+	}
+	public String getBoardNumber() {
+		return BoardNumber;
+	}
+	public void setBoardNumber(String boardNumber) {
+		BoardNumber = boardNumber;
+	}
+	public String getBoardTitle() {
+		return BoardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		BoardTitle = boardTitle;
+	}
+	public String getBoardComm() {
+		return BoardComm;
+	}
+	public void setBoardComm(String boardComm) {
+		BoardComm = boardComm;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyUser() {
+		return modifyUser;
+	}
+	public void setModifyUser(String modifyUser) {
+		this.modifyUser = modifyUser;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	
 }
